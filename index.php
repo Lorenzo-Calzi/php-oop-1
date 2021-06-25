b5d9448caa0ff490ad59cce018cdb8948d7293e3
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-06-25 14:05:27 +0200

    Aggiunto costruttore

--- index.php
+++ index.php
@@ -6,5 +6,13 @@
         public $language;
         public $genre;
         public $desc;
+
+        function __construct(string $name, string $language, string $genre, string $desc)
+        {
+            $this->name = $name;
+            $this->language = $language;
+            $this->genre = $genre;
+            $this->desc = $desc;
+        }
     }
 ?>
